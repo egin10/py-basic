e6f8d675c7ae4a273777ad7c499bf9861638f8e4
egin10 <egin066@gmail.com>
2017-09-24 20:43:50 +0700

    init commit

---
+++ hello.py
@@ -0,0 +1,57 @@
+# print "Hallo Gaes"
+
+# name = 'Ginanjar'
+# i = 20
+# print "Nama saya",name,"dan saya berumur",i
+
+# i = 1
+# if (i < 10) :
+#     print ("nilai yang anda masukan kurang dari 10")
+# elif (i > 50):
+#     print ("nilai yang anda masukan lebih dari 50")
+# else :
+#     print ("nilai yang anda masukan lebih dari 10")
+
+# i = 1
+# while (i < 10) :
+#     print i
+#     i += 1
+
+# angka = [1, 2, 3, 4]
+# for i in angka :
+#     print i
+
+# nama = ["wahid", "egin", "siska"]
+# for n in nama :
+#     print n
+
+# n = 0
+# while (n < 20) :
+#     n += 1
+#     if((n % 3 == 0) & (n % 5 == 0)):
+#         print 'PT SYDECO'
+#     elif (n % 5 == 0):
+#         print 'SYDECO'
+#     elif (n % 3 == 0):
+#         print 'PT'
+#     else:
+#         print n
+#     # print n
+
+# kendaraan = ['mobil', 'motor', 'sepeda', 'bus']
+# for data in kendaraan :
+#     print "Nama kendaraan : %s" %data
+
+# n = len(kendaraan) - 1
+# i = 0
+# while ( i < n ) :
+#     i += 1
+#     print "%s" %kendaraan[i]
+
+# function
+# def greeting(nama) :
+#     print "Hello %s!" %nama
+
+# greeting("Egin")
+
+print "Good Bye!"
