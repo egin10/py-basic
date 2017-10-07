0eccd7d1a4f01fb676c1fb34d4e0d76a6468cec5
egin10 <egin066@gmail.com>
2017-10-07 20:37:26 +0700

    module

--- mod/obj.py
+++ mod/obj.py
@@ -1,6 +1,8 @@
 class Orang:
     def intro(self, nama):
+        "perkenalan diri"
         print "Hai, saya", nama
     
     def pekerjaan(self, job):
+        "info pekerjaan"
         print "Saya bekerja di", job
